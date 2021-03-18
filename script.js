8897f6274f251c1a3d7e64fd20d7efe265dd909d
Hyeongseok Yang <60818951+hyeongseok1001@users.noreply.github.com>
2021-03-18 12:01:43 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 'use strict'
 
- 
+const menuStacks = document.querySelector('.menu__stacks');
+const subMenu = document.querySelector('.subMenu');
+const intro = document.querySelectorAll('.intro');
+const menu = document.querySelector('.menu');
+const header = document.querySelector('header');
+
+menuStacks.addEventListener('mouseenter', (e) => {
+    for (let item of intro) {
+        item.style.display = 'none';
+    }
+    subMenu.style.display = 'flex';
+
+    //console.log(e);
+})
+
+header.addEventListener('mouseleave', (e) => {
+    //header.style.backgroundColor = 'rgb(243, 255, 200)';
+    for (let item of intro) {
+        item.style.display = 'block';
+    }
+    subMenu.style.display = 'none';
+
+    //console.log(e);
+})
